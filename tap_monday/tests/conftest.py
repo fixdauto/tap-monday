cfde4ebc99256b3464112747ce0380396656fac6
Maksim Sundukov <e-xormail@tut.by>
2022-06-02 21:17:14 -0400

    More tests

---
+++ tap_monday/tests/conftest.py
@@ -0,0 +1,130 @@
+import pytest
+
+@pytest.fixture
+def fixture_boards():
+    return {
+        "data": {
+            "boards": [
+                {
+                    "name": "My board",
+                    "id": "2389168662",
+                    "description": None,
+                    "state": "active",
+                    "updated_at": "2022-02-05T00:27:23Z",
+                    "owner": {
+                        "id": 21226602,
+                        "name": "Bat Man",
+                        "email": "batman@batman.com"
+                    },
+                    "workspace": {
+                        "id": 843701,
+                        "name": "Main",
+                        "kind": None,
+                        "description": None
+                    }
+                }
+            ]
+        }
+    }
+
+
+@pytest.fixture
+def fixture_groups():
+    return {
+        "data": {
+            "boards": [
+                {
+                    "id": "2389168662",
+                    "groups": [
+                        {
+                            "title": "My group name",
+                            "position": "512.0",
+                            "id": "new_group23604",
+                            "color": "#fdab3d",
+                            "archived": False,
+                            "deleted": False
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+
+@pytest.fixture
+def fixture_items():
+    return {
+        "data": {
+            "items": [
+                {
+                    "id": "2274512428",
+                    "name": "My item name",
+                    "created_at": "2021-10-06T14:31:02Z",
+                    "updated_at": "2021-10-07T14:31:02Z",
+                    "creator_id": "21226602",
+                    "creator": {
+                        "email": "batman@batman.com",
+                        "name": "Bat Man",
+                    },
+                    "state": "active",
+                    "parent_item": None,
+                    "board": {
+                        "id": "2389168662",
+                        "name": "My board",
+                    },
+                    "group": {
+                        "title": "My group name",
+                        "id": "new_group8875",
+                    },
+                }
+            ],
+        }
+    }
+
+
+@pytest.fixture
+def fixture_columns():
+    return {
+        "data": {
+            "boards": [
+                {
+                    "id": "2389168662",
+                    "columns": [
+                        {
+                            "id": "check",
+                            "title": "Approved",
+                            "archived": False,
+                            "settings_str": "{}",
+                            "description": None,
+                            "type": "boolean",
+                            "width": 120,
+                        }
+                    ],
+                }
+            ],
+        }
+    }
+
+
+
+@pytest.fixture
+def fixture_column_values():
+    return {
+        "data": {
+            "items": [
+                {
+                    "id": "2274512428",
+                    "column_values": [
+                        {
+                            "id": "status",
+                            "title": "Status",
+                            "type": "color",
+                            "value": '\"{\\"index\\":1,\\"post_id\\":null,\\"changed_at\\":\\"2022-04-14T19:07:18.872Z\\"}\"',
+                            "text": "Done",
+                            "additional_info": '\"{\\"label\\":\\"Done\\",\\"color\\":\\"#00c875\\",\\"changed_at\\":\\"2022-04-14T19:07:18.872Z\\"}\"',
+                        }
+                    ],
+                }
+            ],
+        }
+    }
--- tap_monday/tests/test_core.py
+++ tap_monday/tests/test_core.py
@@ -3,7 +3,7 @@ import pytest
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_monday.tap import TapMonday
-from tap_monday.streams import BoardsStream, GroupsStream
+from tap_monday.streams import BoardsStream, GroupsStream, ItemsStream
 
 SAMPLE_CONFIG = {
     "api_url": "mock://api.monday.test/v2",
@@ -12,84 +12,23 @@ SAMPLE_CONFIG = {
 }
 
 
-@pytest.fixture
-def fixture_boards():
-    return {
-        "data": {
-            "boards": [
-                {
-                    "name": "My board name",
-                    "id": "2389168662",
-                    "description": None,
-                    "state": "active",
-                    "updated_at": "2022-02-05T00:27:23Z",
-                }
-            ]
-        }
-    }
-
-
-@pytest.fixture
-def fixture_groups():
-    return {
-        "data": {
-            "boards": [
-                {
-                    "groups": [
-                        {
-                            "title": "My group name",
-                            "position": "512.0",
-                            "id": "new_group23604",
-                        }
-                    ]
-                }
-            ]
-        }
-    }
-
-
-@pytest.fixture
-def fixture_items():
-    return {
-        "data": {
-            "boards": [
-                {
-                    "groups": [
-                        {
-                            "title": "My group name",
-                            "position": "65536.0",
-                            "id": "new_group8875",
-                            "items": [
-                                {
-                                    "name": "My item name",
-                                    "created_at": "2021-10-07T14:31:02Z",
-                                    "column_values": [
-                                        {
-                                            "id": "subitems",
-                                            "title": "Subitems",
-                                            "type": "subtasks",
-                                            "value": None,
-                                            "text": "",
-                                        }
-                                    ],
-                                }
-                            ],
-                        }
-                    ]
-                }
-            ]
-        }
-    }
-
-
-def test_tap_standard(requests_mock, fixture_boards, fixture_groups, fixture_items):
+def test_tap_standard(
+    requests_mock,
+    fixture_boards,
+    fixture_groups,
+    fixture_items,
+    fixture_columns,
+    fixture_column_values
+):
     requests_mock.register_uri(
         "POST",
         SAMPLE_CONFIG["api_url"],
         [
             {"json": fixture_boards, "status_code": 200},
-            {"json": fixture_groups, "status_code": 200},
+            {"json": fixture_columns, "status_code": 200},
             {"json": fixture_items, "status_code": 200},
+            {"json": fixture_column_values, "status_code": 200},
+            {"json": fixture_groups, "status_code": 200},
         ],
     )
     tests = get_standard_tap_tests(TapMonday, config=SAMPLE_CONFIG)
@@ -102,7 +41,7 @@ def test_board_parsing(fixture_boards):
     stream = BoardsStream(tap=tap)
     processed_row = stream.post_process(fixture_boards["data"]["boards"][0])
     assert processed_row["id"] == 2389168662
-    assert processed_row["description"] == "None"
+    assert processed_row["description"] == None
 
 
 def test_group_parsing(fixture_groups):
@@ -113,3 +52,15 @@ def test_group_parsing(fixture_groups):
     )
     assert processed_row["position"] == 512.0
     assert processed_row["board_id"] == 2389168662
+
+def test_items_parsing(fixture_items):
+    tap = TapMonday(config=SAMPLE_CONFIG)
+    stream = ItemsStream(tap=tap)
+    processed_row = stream.post_process(
+        fixture_items["data"]["items"][0]
+    )
+    assert processed_row["board_id"] == 2389168662
+    assert processed_row["parent_item_id"] == 0
+
+# columns
+# column values
